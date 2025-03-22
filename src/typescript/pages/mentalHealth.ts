5697cf8b543780a6899ef426bd8de93002389308
nops <0x9090@users.noreply.github.com>
2025-03-22 09:51:32 -0400

    advice

--- src/typescript/pages/mentalHealth.ts
+++ src/typescript/pages/mentalHealth.ts
@@ -728,7 +728,7 @@ const tips = [
     "If a religion focuses *too* much on the end of the world, then it's very likely being used to create fear to manipulate and control you",
     "Very few people can actually predict a major civil or world war. It's only in hindsight that there are clear start and end points to the conflict. In fact, when the American Civil War started, there were civilians who so underestimated the conflict that they went out to picnic and watch the Battle of Manassas, only to find themselves in the middle of a chaotic and bloody fight, and retreating along with the Union soldiers",
     "Only take advice on relationships and love from couples who've been together for a long time. The internet is one of the last places you want to look for that information",
-    "Sometimes the best thing to do is just laugh at the haters. It's possible that it will drive them so crazy, that they will make a mistake that you can use against them",
+    "Sometimes the best thing to do is just laugh at the haters. It's possible that it will drive them so crazy, that they will make a mistake that you can use against them. As long as you're prepared to deal with the lash out, then let them make that mistake",
 
 
 
