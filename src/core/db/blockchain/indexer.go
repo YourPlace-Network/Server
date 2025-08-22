061cfb25a2d3e1b0895627cf795fdc97b38a2b92
rwos1913 <currycarr063@gmail.com>
2025-08-22 11:14:30 -0400

    indexer fix

--- src/core/db/blockchain/indexer.go
+++ src/core/db/blockchain/indexer.go
@@ -1237,6 +1237,10 @@ func handlePostTransactionAttachment(payloadObject map[string]interface{}, txHas
 			core.LogDebug("Post attach action fields are not array")
 			return false
 		}
+		if len(attachmentArray) != 4 {
+			core.LogDebug("attachment array length is not 4")
+			return false
+		}
 		parsedURL, okURL := attachmentArray[0].(string)
 		parsedMimeType, okMimeType := attachmentArray[1].(string)
 		sizeFloat, okSize := attachmentArray[2].(float64)
