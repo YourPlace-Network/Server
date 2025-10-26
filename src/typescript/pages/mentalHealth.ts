c96bee6fedaefb620e9200cd798b837e0d14e42d
nops <0x9090@users.noreply.github.com>
2025-10-26 18:16:42 +0700

    advice

--- src/typescript/pages/mentalHealth.ts
+++ src/typescript/pages/mentalHealth.ts
@@ -968,8 +968,8 @@ const tips = [
     "Do not move to a small town if you're single. Move to the nearest cool college town or regional city",
     "Every time humans pursue ambitious projects like space exploration or nuclear energy, we end up with second-order and sometime unexpected benefits",
     "Ladies: Don't get too comfortable getting in a mans face to try and intimidate him. Your privilege only extends so far, and men largely have a monopoly on violence. He might go to jail if he hits you, but you have a very high risk of being permanently disfigured or dead if that happens. Record and report from a distance so the good men can fight the bad ones. Because history says the bad ones will cave in your face and not think twice. And that can happen in a split second, even if you're in public and surrounded by friends",
-
-
+    "Most people don't really have principals, they just want power. But they'll have no problem using your own principals against you - don't let them",
+    "If you're over 70 years old, you need to stop trying to change society and instead look for the younger person to pass your power to. It's a huge societal problem when the older generation won't step aside. And at some point, the young need to exert their own power and force them out",
 
 ];
 
