1f0a17139c762adc593eb87e8ed617dbbcc40b4e
nops <0x9090@users.noreply.github.com>
2025-11-04 11:50:39 +0700

    gateway deployment

--- src/typescript/pages/mentalHealth.ts
+++ src/typescript/pages/mentalHealth.ts
@@ -981,7 +981,7 @@ const tips = [
     "Listen to what people do, and not what they say",
     "The most guilty person is usually the one who is first to claim \"we all made mistakes.\" They're just trying to muddy the waters and deflect as much blame as possible",
     "Don't fall for the doomer nonsense. That's for losers and normies scared of their own shadows. Walk around like God sent you and smile at everyone you see. Spread light and abundance. Build things and take chances",
-
+    "The truth is, remote work only works for a small set of people. Most people will be lazy by default when unsupervised and will just do the bare minimum",
 
 ];
 
