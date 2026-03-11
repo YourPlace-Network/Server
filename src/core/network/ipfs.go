e974c53b95be88398e7a695dfa6f0f1f65c35886
nops <0x9090@users.noreply.github.com>
2026-03-11 11:45:41 +0700

    hardcoding the NFT group ID on pinata

--- src/core/network/ipfs.go
+++ src/core/network/ipfs.go
@@ -500,40 +500,7 @@ func PinningServiceGenerateUploadAuth(ps *PinningService) (map[string]string, er
 		"uploadUrl": signResult.Data,
 	}, nil
 }
-func PinningServiceLookupGroupID(ps *PinningService, groupName string) (string, error) {
-	client := &http.Client{Timeout: 15 * time.Second}
-	req, err := http.NewRequest("GET", pinataAPIURL+"/v3/groups/public?name="+groupName, nil)
-	if err != nil {
-		return "", _core.LogDebugReturn("Could not create Pinata group lookup request: " + err.Error())
-	}
-	req.Header.Set("Authorization", "Bearer "+ps.Key)
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", _core.LogDebugReturn("Could not look up Pinata group: " + err.Error())
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", _core.LogDebugReturn("Could not read Pinata group response: " + err.Error())
-	}
-	if resp.StatusCode != http.StatusOK {
-		return "", _core.LogDebugReturn("Pinata group lookup returned status " + strconv.Itoa(resp.StatusCode) + ": " + string(body))
-	}
-	var groupResult struct {
-		Data struct {
-			Groups []struct {
-				ID string `json:"id"`
-			} `json:"groups"`
-		} `json:"data"`
-	}
-	if err = json.Unmarshal(body, &groupResult); err != nil {
-		return "", _core.LogDebugReturn("Could not parse Pinata group response: " + err.Error())
-	}
-	if len(groupResult.Data.Groups) == 0 || groupResult.Data.Groups[0].ID == "" {
-		return "", _core.LogDebugReturn("Pinata group '" + groupName + "' not found")
-	}
-	return groupResult.Data.Groups[0].ID, nil
-}
+
 func PinningServiceInit(pinningType string, pinningURL string, key string) (*PinningService, error) {
 	if pinningType != "pinata" && pinningType != "ipfs" {
 		return nil, _core.LogErrorReturn("Invalid pinning service type: " + pinningType + " (must be 'pinata' or 'ipfs')")
